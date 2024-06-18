0a4500a9e2ce3a7a64ceb4abdccc050262016769
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-06-18 16:20:30 -0300

    fix menu mobile reponssivity

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -21,35 +21,38 @@ export const Header = () => {
             <div className="hidden lg:block bg-yellow-rubraz rounded-full w-full h-[73px] absolute top-5 -right-[50%] -z-10"></div>
             
             <div className="flex lg:px-4 px-6 gap-4 text-lg flex-nowrap lg:flex-nowrap h-full w-full ">
-              <div className="flex flex-col lg:w-1/3 w-2/3 items-center justify-center ">
+              <div className="flex lg:flex-col gap-4 lg:gap-0 lg:w-1/3 w-full md:w-[70%] items-center justify-between lg:justify-center ">
                 <a className='flex gap-2 items-center justify-center ' href='https://api.whatsapp.com/send/?phone=5521979808794&text=Ol%C3%A1%2C+venho+atrav%C3%A9s+do+site+e+gostaria+de+or%C3%A7amento.+Poderia+me+ajudar%3F&type=phone_number&app_absent=0' target="_blank">
                   <div className="rounded-full lg:w-[38px] flex justify-center items-center ">
                     <img src={wppIcon} alt='Icone de whatsApp' className='text-white md:w-[40px] w-[30px] '/>
                   </div>
                   <span className='md:text-base text-[14px]'>21 97980-8794</span>
                 </a>
+                <div className='lg:hidden flex justify-center'>
+                  {isOpen ? (
+                      <FaTimes className='md:text-2xl lg:hidden' onClick={() => setIsOpen(!isOpen)} />
+                    ) : (
+                      <img src={hamburguerIcon} alt='icone menu hamburguer' className='md:w-[42px] w-[32px] lg:hidden' onClick={() => setIsOpen(!isOpen)}/>
+                    )}
+
+                    {isOpen && (
+                      <div className="absolute top-24 right-auto right-0.5">
+                        <div className="bg-white shadow-md rounded-lg py-4 px-6 mt-2 relative z-20">
+                          <div className="absolute top-[-10px] left-[50%] transform -translate-x-1/2 w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-b-[10px] border-b-white"></div>
+                          <ul className="flex flex-col gap-4">
+                            <li><a href='https://www.facebook.com/rubrazlajes' target='_blank' className='text-blue-rubraz flex items-center gap-2 md:text-base text-sm'><FaFacebookF className='md:text-2xl text-sm'/> Facebook</a></li>
+                            <li><a href='https://www.instagram.com/rubrazlajes/' target='_blank' className='text-blue-rubraz flex items-center gap-2 md:text-base text-sm'><FaInstagram className='md:text-2xl text-sm'/> Instagram</a></li>
+                            <li><a href='https://rubrazlajes.com/' target="_blank" className='text-blue-rubraz flex items-center gap-2 md:text-base text-sm'>rubrazlajes.com</a></li>
+                          </ul>
+                        </div>
+                      </div>
+                    )}
+                </div>
+               
               </div>
-              <div className="flex flex-col lg:w-1/3 w-1/8 items-center lg:items-end justify-center ">
+              <div className="hidden lg:flex flex-col lg:w-1/3 w-1/8 items-center lg:items-end justify-center ">
                 <a href='https://rubrazlajes.com/' target="_blank" className='items-center hidden lg:block'>rubrazlajes.com</a>
                 
-                  {isOpen ? (
-                    <FaTimes className='md:text-2xl lg:hidden' onClick={() => setIsOpen(!isOpen)} />
-                  ) : (
-                    <img src={hamburguerIcon} alt='icone menu hamburguer' className='md:w-[42px] w-[32px] lg:hidden' onClick={() => setIsOpen(!isOpen)}/>
-                  )}
-
-                  {isOpen && (
-                    <div className="absolute top-24 ">
-                      <div className="bg-white shadow-md rounded-lg py-4 px-6 mt-2 relative z-20">
-                        <div className="absolute top-[-10px] left-[50%] transform -translate-x-1/2 w-0 h-0 border-l-[10px] border-l-transparent border-r-[10px] border-r-transparent border-b-[10px] border-b-white"></div>
-                        <ul className="flex flex-col gap-4">
-                          <li><a href='https://www.facebook.com/rubrazlajes' target='_blank' className='text-blue-rubraz flex items-center gap-2'><FaFacebookF className='text-2xl'/> Facebook</a></li>
-                          <li><a href='https://www.instagram.com/rubrazlajes/' target='_blank' className='text-blue-rubraz flex items-center gap-2'><FaInstagram className='text-2xl'/> Instagram</a></li>
-                          <li><a href='https://rubrazlajes.com/' target="_blank" className='text-blue-rubraz flex items-center gap-2'>rubrazlajes.com</a></li>
-                        </ul>
-                      </div>
-                    </div>
-                  )}
              
               </div>
               <div className="hidden lg:flex flex-col w-1/3 items-end justify-center">
