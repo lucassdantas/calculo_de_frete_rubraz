ef6ab88f4d6185ac9bff00bcd5598c582f5c4fea
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-06-18 16:25:33 -0300

    build changes

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 export const Footer = () => {
   return (
     <>
-      < div className="lg:hidden lg:invisible flex justify-center gap-6 text-base bg-yellow-rubraz text-black  p-6  text-left w-full z-20 relative ">
+      < div className="lg:hidden lg:invisible flex flex-col  justify-center items-center text-base bg-yellow-rubraz text-black  p-2 w-full z-20 relative ">
+        <div>
           <p><span className='font-bold'>CNPJ: </span>22.577.009/0001-00 </p>
           <p><span className='font-bold'>CREA-RJ: </span>2023200826 </p>
+        </div>
       </div>
       <footer className='w-full bg-black text-center text-white py-4 text-sm z-20 relative px-4'>
         Copyright © 2024 Rubraz Lajes & Concretos. Todos os direitos reservados. | Desenvolvido por R&D Marketing Digital. Agência de Marketing Digital.
