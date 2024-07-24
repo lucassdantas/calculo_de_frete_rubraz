8dee6172d2cce61967074d7c18690bdf19993a53
Lucas Dantas <lucasdantasprogramador@gmail.com>
2024-07-24 08:56:31 -0300

    fix popUp

--- src/components/UserPopup/index.tsx
+++ src/components/UserPopup/index.tsx
@@ -88,7 +88,7 @@ const UserPopup: React.FC<UserPopupProps> = ({ onClose }) => {
 
   return (
     <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50 z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg relative w-full max-w-md h-full max-h-[80vh]" ref={popupRef}>
+      <div className="bg-white p-6 rounded-lg shadow-lg relative w-full max-w-md h-full max-h-[90vh] overflow-y-scroll" ref={popupRef}>
         <button
           onClick={onClose}
           className="absolute top-2 right-2 text-gray-600 hover:text-gray-800"
